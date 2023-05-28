a5ecddfd645c4e6a8c9f34a7c1aa76df39194273
Imrangithubrit <devopsandaws2023@gmail.com>
2023-05-28 20:44:21 +0530

    donedbw

--- Mongodb.sh
+++ Mongodb.sh
@@ -1,5 +1,5 @@
 echo -e "\e[33mCopying for Mongd.repo file \e[0m"
-cp Mongod.repo /etc/yum.repos.d/mongodb.repo
+cp mongod.repo /etc/yum.repos.d/mongodb.repo
 echo -e "\e[33m Installing the Mongodb file \e[0m"
 
 yum install mongodb-org -y 
