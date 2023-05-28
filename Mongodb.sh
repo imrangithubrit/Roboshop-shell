e3dddedee98678d540e0d2356ba076977caa5873
Imrangithubrit <devopsandaws2023@gmail.com>
2023-05-28 21:05:14 +0530

    color for mongo

--- Mongodb.sh
+++ Mongodb.sh
@@ -1,13 +1,15 @@
 echo -e "\e[33mCopying for Mongd.repo file \e[0m"
-cp Mongod.repo /etc/yum.repos.d/mongodb.repo
+cp Mongod.repo /etc/yum.repos.d/mongodb.repo   &>>/tmp/roboshop.log
 echo -e "\e[33m Installing the Mongodb file \e[0m"
 
-yum install mongodb-org -y 
+yum install mongodb-org -y   &>>/tmp/roboshop.log
 
 echo -e "\e[33m Enabling the fie \e[0m"
-systemctl enable mongod 
+systemctl enable mongod   &>>/tmp/roboshop.log
 
 
 echo -e "\e[33m restarting the mongodb \e[0m"
 
-systemctl restart mongod
+
+systemctl restart mongod   &>>/tmp/roboshop.log
+ 
