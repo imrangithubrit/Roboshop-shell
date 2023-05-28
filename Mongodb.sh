b5a2d0b880a8ae49f1a4f63545ea5e4996c3abad
Imrangithubrit <devopsandaws2023@gmail.com>
2023-05-28 20:38:43 +0530

    mong[piosd

---
+++ Mongodb.sh
@@ -0,0 +1,13 @@
+echo -e "\e[33mCopying for Mongd.repo file \e[0m"
+cp mongodb.repo /etc/yum.repos.d/mongodb.repo
+echo -e "\e[33m Installing the Mongodb file \e[0m"
+
+yum install mongodb-org -y 
+
+echo -e "\e[33m Enabling the fie \e[0m"
+systemctl enable mongod 
+
+
+echo -e "e[33m restarting the mongodb \e[0m"
+
+systemctl restart mongod
